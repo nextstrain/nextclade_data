7558baf5e08f3748c158ededc80c366fce0e1eab
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-09-01 14:01:38 +0200

    feat: output minimizers in json format

--- scripts/lib/minimizer.py
+++ scripts/lib/minimizer.py
@@ -1,8 +1,21 @@
+import copy
+
 from Bio.SeqRecord import SeqRecord
 import numpy as np
 
+# Increment this version, if there are changes in the algorithm.
+# Backwards compatibility must be ensured to not break client code: all versions must be computed and reffered to
+# from the dataset's server 'index.json' file.
+MINIMIZER_ALGO_VERSION = "1"
+
+# Increment this version, if there are changes in the layout of the output file.
+MINIMIZER_JSON_SCHEMA_VERSION = "3.0.0"
+
+# What is this?
+MAGIC_NUMBER_K = 17
+
 # minimizer cutoff. The max is 1<<32 - 1, so with 28 uses roughly 1/16 of all kmers
-cutoff = 1 << 28
+CUTOFF = 1 << 28
 
 
 # from lh3
@@ -25,7 +38,7 @@ def get_hash(kmer):
   for i, nuc in enumerate(kmer):
     if i % 3 == 2: continue  # skip every third nucleotide to pick up conserved patterns
     if nuc not in 'ACGT':
-      return cutoff + 1  # break out of loop, return hash above cutoff
+      return CUTOFF + 1  # break out of loop, return hash above cutoff
     else:  # A=11=3, C=10=2, G=00=0, T=01=1
       if nuc in 'AC':
         x += 1 << j
@@ -36,14 +49,14 @@ def get_hash(kmer):
   return invertible_hash(x)
 
 
-def get_ref_search_minimizers(seq: SeqRecord, k=17):
+def get_ref_search_minimizers(seq: SeqRecord, k=MAGIC_NUMBER_K):
   seq_str = preprocess_seq(seq)
   minimizers = []
   # we know the rough number of minimizers, so we can pre-allocate the array if needed
   for i in range(len(seq_str) - k):
     kmer = seq_str[i:i + k]
     mhash = get_hash(kmer)
-    if mhash < cutoff:  # accept only hashes below cutoff --> reduces the size of the index and the number of look-ups
+    if mhash < CUTOFF:  # accept only hashes below cutoff --> reduces the size of the index and the number of look-ups
       minimizers.append(mhash)
   return np.unique(minimizers)
 
@@ -74,8 +87,26 @@ def make_ref_search_index(refs):
     # reference will be a list in same order as the bit set
     index["references"].append(minimizer_set['meta'])
 
-  return index
+  normalization = np.array([x['length'] / x['n_minimizers'] for x in index["references"]])
+
+  return {
+    "schemaVersion": MINIMIZER_JSON_SCHEMA_VERSION,
+    "version": MINIMIZER_ALGO_VERSION,
+    "params": {
+      "k": MAGIC_NUMBER_K,
+      "cutoff": CUTOFF,
+    },
+    **index,
+    "normalization": normalization
+  }
 
 
 def preprocess_seq(seq: SeqRecord) -> str:
   return str(seq.seq).upper().replace('-', '')
+
+
+def serialize_ref_search_index(index):
+  index = copy.deepcopy(index)
+  index["minimizers"] = {int(k): v.tolist() for k, v in index["minimizers"].items()}
+  index["normalization"] = index["normalization"].tolist()
+  return index
