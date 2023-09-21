e883ffe0cff049ec9954a88de7b7c289f33c4651
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-09-21 18:15:21 +0200

    Update release script to work from staging until v3 is out

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,19 +2,10 @@
 # Make sure that you local machine has the latest changes from the remote server
 git pull --all
 
-# Switch to release branch
-git checkout staging
-
-# Fast-forward release branch to master locally
-git merge master staging --ff-only
-
-# Push release branch to the remote server
-git push origin staging
-
 git checkout release
-git merge master release --ff-only
+git merge staging release --ff-only
 git push origin release
 
 # Switch away from the release branch to avoid accidental commits later
-git checkout master
+git checkout staging
 
