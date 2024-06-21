0e4c9fad103914e15d54a8664afa2d6f84295eb1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-06-21 10:38:48 +0200

    add skipAsReference fields
    See https://github.com/nextstrain/nextclade/pull/1492/commits/7c60909f8aa2d75ddd2b07cb7e08562a5b7fdf67

--- scripts/migrate_006_add_rel_muts.py
+++ scripts/migrate_006_add_rel_muts.py
@@ -10,282 +10,333 @@ Replace .meta.extensions.nextclade.ref_nodes" in all tree.json files matching a 
 """
 
 # Map from subdirectory path(s) to the ref_nodes object
-SUBDIRS = {
+DATA = {
   "nextstrain/sars-cov-2": {
-    "default": "__root__",
-    "search": [
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "clade-founder-by-clade",
+          "displayName": "Clade founder (by node clade)",
+          "description": "Mutations relative to founder of clade (earliest ancestor by clade)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["20A"]}],
+              "node": [{"clade": ["20A"], "searchAlgo": "ancestor-earliest"}],
+            },
+            {
+              "qry": [{"clade": ["23A"]}],
+              "node": [{"clade": ["23A"], "searchAlgo": "ancestor-earliest"}],
+            },
+            {
+              "qry": [{"clade": ["23B"]}],
+              "node": [{"clade": ["23B"], "searchAlgo": "ancestor-earliest"}],
+            }
+          ]
+        },
+        {
+          "name": "clade-founder-by-name",
+          "displayName": "Clade founder (by node name)",
+          "description": "Mutations relative to founder of clade (full search by name)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["20A"]}],
+              "node": [{"name": ["NODE_0000000"]}],
+            },
+            {
+              "qry": [{"clade": ["23A"]}],
+              "node": [{"name": ["XBB.1.5"]}],
+            },
+            {
+              "qry": [{"clade": ["23B"]}],
+              "node": [{"name": ["NODE_0000862"]}],
+            },
+          ]
+        },
+        {
+          "name": "22B",
+          "displayName": "22B",
+          "description": "Earliest ancestor having clade 22B (only for samples having clade 22B)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["22B"]}],
+              "node": [{"clade": ["22B"], "searchAlgo": "ancestor-earliest"}],
+            }
+          ]
+        },
+        {
+          "name": "BA.2.86",
+          "displayName": "BA.2.86 (23I)",
+          "description": "Full search by name: NODE_0000659 (only for samples having clade 22I)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["23I"]}],
+              "node": [{"name": ["NODE_0000659"]}],
+            }
+          ],
+        },
+        {
+          "name": "XBB.1.5",
+          "displayName": "XBB.1.5 (23A)",
+          "description": "Full search by name: XBB.1.5 (only for samples having clade 23A)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["23A"]}],
+              "node": [{"name": ["XBB.1.5"]}],
+            }
+          ],
+        },
+        {
+          "name": "BA.5",
+          "displayName": "BA.5 (22B)",
+          "description": "Full search by name: NODE_0000862 (only for samples having clade 22B)",
+          "criteria": [
+            {
+              "qry": [{"clade": ["22B"]}],
+              "node": [{"name": ["NODE_0000862"]}],
+            }
+          ],
+        }
+      ]
+    },
+    "clade_node_attrs": [
       {
-        "name": "clade-founder-by-clade",
-        "displayName": "Clade founder (by node clade)",
-        "description": "Mutations relative to founder of clade (earliest ancestor by clade)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["20A"]}],
-            "node": [{"clade": ["20A"], "searchAlgo": "ancestor-earliest"}],
-          },
-          {
-            "qry": [{"clade": ["23A"]}],
-            "node": [{"clade": ["23A"], "searchAlgo": "ancestor-earliest"}],
-          },
-          {
-            "qry": [{"clade": ["23B"]}],
-            "node": [{"clade": ["23B"], "searchAlgo": "ancestor-earliest"}],
-          }
-        ]
+        "name": "Nextclade_pango",
+        "displayName": "Pango lineage (Nextclade)",
+        "description": "Pango lineage as inferred by Nextclade from the nearest neighbour in the reference tree. 98% "
+                       "accurate for recent sequences, for higher accuracy use dedicated pangolin software in UShER "
+                       "or pangoLEARN mode. Recombinants may get (wrongly) assigned to a designated recombinant "
+                       "lineage if they have similar breakpoints.",
+        "hideInWeb": False
       },
       {
-        "name": "clade-founder-by-name",
-        "displayName": "Clade founder (by node name)",
-        "description": "Mutations relative to founder of clade (full search by name)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["20A"]}],
-            "node": [{"name": ["NODE_0000000"]}],
-          },
-          {
-            "qry": [{"clade": ["23A"]}],
-            "node": [{"name": ["XBB.1.5"]}],
-          },
-          {
-            "qry": [{"clade": ["23B"]}],
-            "node": [{"name": ["NODE_0000862"]}],
-          },
-        ]
+        "name": "partiallyAliased",
+        "displayName": "Unaliased",
+        "description": "Partially aliased reconstructed Pango lineage",
+        "hideInWeb": False,
+        "skipAsReference": True
       },
       {
-        "name": "22B",
-        "displayName": "22B",
-        "description": "Earliest ancestor having clade 22B (only for samples having clade 22B)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["22B"]}],
-            "node": [{"clade": ["22B"], "searchAlgo": "ancestor-earliest"}],
-          }
-        ]
+        "name": "clade_nextstrain",
+        "displayName": "Nextstrain Clade",
+        "description": "Nextstrain Clade",
+        "hideInWeb": True,
+        "skipAsReference": True
       },
       {
-        "name": "BA.2.86",
-        "displayName": "BA.2.86 (23I)",
-        "description": "Full search by name: NODE_0000659 (only for samples having clade 22I)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["23I"]}],
-            "node": [{"name": ["NODE_0000659"]}],
-          }
-        ],
+        "name": "clade_who",
+        "displayName": "WHO name",
+        "description": "Greek letter WHO name",
+        "hideInWeb": True,
+        "skipAsReference": False
       },
       {
-        "name": "XBB.1.5",
-        "displayName": "XBB.1.5 (23A)",
-        "description": "Full search by name: XBB.1.5 (only for samples having clade 23A)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["23A"]}],
-            "node": [{"name": ["XBB.1.5"]}],
-          }
-        ],
-      },
-      {
-        "name": "BA.5",
-        "displayName": "BA.5 (22B)",
-        "description": "Full search by name: NODE_0000862 (only for samples having clade 22B)",
-        "criteria": [
-          {
-            "qry": [{"clade": ["22B"]}],
-            "node": [{"name": ["NODE_0000862"]}],
-          }
-        ],
+        "name": "clade_display",
+        "displayName": "Clade display name",
+        "description": "Combination of Nextstrain clade and Pango lineage",
+        "hideInWeb": True,
+        "skipAsReference": True
       }
     ]
   },
   "nextstrain/flu/h1n1pdm/ha/CY121680": {
-    "default": "__root__",
-    "search": [
-      {
-        "name": "A/Wisconsin/67/2022",
-        "displayName": "A/Wisconsin/67/2022",
-        "description": "Isolate first used in vaccine for NH season 2023/24",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Wisconsin/67/2022"]}],
-          }
-        ],
-      },
-      {
-        "name": "A/Sydney/5/2021",
-        "displayName": "A/Sydney/5/2021",
-        "description": "Isolate first used in vaccine for SH season 2023",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Sydney/5/2021"]}],
-          }
-        ],
-      },
-      {
-        "name": "A/Wisconsin/588/2019",
-        "displayName": "A/Wisconsin/588/2019",
-        "description": "Isolate first used in vaccine for SH season 2021",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Wisconsin/588/2019"]}],
-          }
-        ],
-      }
-    ]
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "A/Wisconsin/67/2022",
+          "displayName": "A/Wisconsin/67/2022",
+          "description": "Isolate first used in vaccine for NH season 2023/24",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Wisconsin/67/2022"]}],
+            }
+          ],
+        },
+        {
+          "name": "A/Sydney/5/2021",
+          "displayName": "A/Sydney/5/2021",
+          "description": "Isolate first used in vaccine for SH season 2023",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Sydney/5/2021"]}],
+            }
+          ],
+        },
+        {
+          "name": "A/Wisconsin/588/2019",
+          "displayName": "A/Wisconsin/588/2019",
+          "description": "Isolate first used in vaccine for SH season 2021",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Wisconsin/588/2019"]}],
+            }
+          ],
+        }
+      ]
+    }
   },
   ("nextstrain/flu/h1n1pdm/ha/MW626062", "nextstrain/flu/h1n1pdm/na/MW626056"): {
-    "default": "__root__",
-    "search": [
-      {
-        "name": "A/Wisconsin/67/2022",
-        "displayName": "A/Wisconsin/67/2022",
-        "description": "Isolate first used in vaccine for NH season 2023/24",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Wisconsin/67/2022"]}],
-          }
-        ]
-      },
-      {
-        "name": "A/Sydney/5/2021",
-        "displayName": "A/Sydney/5/2021",
-        "description": "Isolate first used in vaccine for SH season 2023",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Sydney/5/2021"]}],
-          }
-        ]
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "A/Wisconsin/67/2022",
+          "displayName": "A/Wisconsin/67/2022",
+          "description": "Isolate first used in vaccine for NH season 2023/24",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Wisconsin/67/2022"]}],
+            }
+          ]
+        },
+        {
+          "name": "A/Sydney/5/2021",
+          "displayName": "A/Sydney/5/2021",
+          "description": "Isolate first used in vaccine for SH season 2023",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Sydney/5/2021"]}],
+            }
+          ]
 
-      },
-      {
-        "name": "A/Wisconsin/588/2019",
-        "displayName": "A/Wisconsin/588/2019",
-        "description": "Isolate first used in vaccine for SH season 2021",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Wisconsin/588/2019"]}],
-          }
-        ]
-      }
-    ]
+        },
+        {
+          "name": "A/Wisconsin/588/2019",
+          "displayName": "A/Wisconsin/588/2019",
+          "description": "Isolate first used in vaccine for SH season 2021",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Wisconsin/588/2019"]}],
+            }
+          ]
+        }
+      ]
+    }
   },
   ("nextstrain/flu/h3n2/ha/EPI1857216", "nextstrain/flu/h3n2/na/EPI1857216"): {
-    "default": "__root__",
-    "search": [
-      {
-        "name": "A/Massachusetts/18/2022",
-        "displayName": "A/Massachusetts/18/2022",
-        "description": "Isolate first used in vaccine for SH season 2024",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Massachusetts/18/2022"]}],
-          }
-        ]
-      },
-      {
-        "name": "A/Darwin/6/2021",
-        "displayName": "A/Darwin/6/2021",
-        "description": "Isolate first used in vaccine for SH season 2022",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Darwin/6/2021"]}],
-          }
-        ]
-      },
-      {
-        "name": "A/HongKong/45/2019",
-        "displayName": "A/HongKong/45/2019",
-        "description": "Isolate first used in vaccine for NH season 2020/2021",
-        "criteria": [
-          {
-            "node": [{"name": ["A/HongKong/45/2019"]}],
-          }
-        ]
-      }
-    ]
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "A/Massachusetts/18/2022",
+          "displayName": "A/Massachusetts/18/2022",
+          "description": "Isolate first used in vaccine for SH season 2024",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Massachusetts/18/2022"]}],
+            }
+          ]
+        },
+        {
+          "name": "A/Darwin/6/2021",
+          "displayName": "A/Darwin/6/2021",
+          "description": "Isolate first used in vaccine for SH season 2022",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Darwin/6/2021"]}],
+            }
+          ]
+        },
+        {
+          "name": "A/HongKong/45/2019",
+          "displayName": "A/HongKong/45/2019",
+          "description": "Isolate first used in vaccine for NH season 2020/2021",
+          "criteria": [
+            {
+              "node": [{"name": ["A/HongKong/45/2019"]}],
+            }
+          ]
+        }
+      ]
+    }
   },
   ("nextstrain/flu/vic/ha/KX058884", "nextstrain/flu/vic/na/KX058884"): {
-    "default": "__root__",
-    "search": [
-      {
-        "name": "B/Austria/1359417/2021",
-        "displayName": "B/Austria/1359417/2021",
-        "description": "Isolate first used in vaccine for SH season 2022",
-        "criteria": [
-          {
-            "node": [{"name": ["B/Austria/1359417/2021"]}],
-          }
-        ]
-      },
-      {
-        "name": "B/Washington/2/2019",
-        "displayName": "B/Washington/2/2019",
-        "description": "Isolate first used in vaccine for SH season 2020",
-        "criteria": [
-          {
-            "node": [{"name": ["B/Washington/2/2019"]}],
-          }
-        ]
-      },
-      {
-        "name": "B/Colorado/6/2017",
-        "displayName": "B/Colorado/6/2017",
-        "description": "Isolate first used in vaccine for NH season 2018/2019",
-        "criteria": [
-          {
-            "node": [{"name": ["B/Colorado/6/2017"]}],
-          }
-        ]
-      }
-    ]
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "B/Austria/1359417/2021",
+          "displayName": "B/Austria/1359417/2021",
+          "description": "Isolate first used in vaccine for SH season 2022",
+          "criteria": [
+            {
+              "node": [{"name": ["B/Austria/1359417/2021"]}],
+            }
+          ]
+        },
+        {
+          "name": "B/Washington/2/2019",
+          "displayName": "B/Washington/2/2019",
+          "description": "Isolate first used in vaccine for SH season 2020",
+          "criteria": [
+            {
+              "node": [{"name": ["B/Washington/2/2019"]}],
+            }
+          ]
+        },
+        {
+          "name": "B/Colorado/6/2017",
+          "displayName": "B/Colorado/6/2017",
+          "description": "Isolate first used in vaccine for NH season 2018/2019",
+          "criteria": [
+            {
+              "node": [{"name": ["B/Colorado/6/2017"]}],
+            }
+          ]
+        }
+      ]
+    }
   },
   ("nextstrain/flu/h3n2/ha/CY163680"): {
-    "default": "__root__",
-    "search": [
-      {
-        "name": "A/Massachusetts/18/2022",
-        "displayName": "A/Massachusetts/18/2022",
-        "description": "Isolate first used in vaccine for SH season 2024",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Massachusetts/18/2022"]}],
-          }
-        ]
-      },
-      {
-        "name": "A/Darwin/6/2021",
-        "displayName": "A/Darwin/6/2021",
-        "description": "Isolate first used in vaccine for SH season 2022",
-        "criteria": [
-          {
-            "node": [{"name": ["A/Darwin/6/2021"]}],
-          }
-        ]
-      },
-      {
-        "name": "A/HongKong/45/2019",
-        "displayName": "A/HongKong/45/2019",
-        "description": "Isolate first used in vaccine for NH season 2020/2021",
-        "criteria": [
-          {
-            "node": [{"name": ["A/HongKong/45/2019"]}],
-          }
-        ]
-      }
-    ]
+    "ref_nodes": {
+      "default": "__root__",
+      "search": [
+        {
+          "name": "A/Massachusetts/18/2022",
+          "displayName": "A/Massachusetts/18/2022",
+          "description": "Isolate first used in vaccine for SH season 2024",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Massachusetts/18/2022"]}],
+            }
+          ]
+        },
+        {
+          "name": "A/Darwin/6/2021",
+          "displayName": "A/Darwin/6/2021",
+          "description": "Isolate first used in vaccine for SH season 2022",
+          "criteria": [
+            {
+              "node": [{"name": ["A/Darwin/6/2021"]}],
+            }
+          ]
+        },
+        {
+          "name": "A/HongKong/45/2019",
+          "displayName": "A/HongKong/45/2019",
+          "description": "Isolate first used in vaccine for NH season 2020/2021",
+          "criteria": [
+            {
+              "node": [{"name": ["A/HongKong/45/2019"]}],
+            }
+          ]
+        }
+      ]
+    }
   }
 }
 
 
 def main():
-  for (subdirs, ref_nodes) in SUBDIRS.items():
+  for (subdirs, data) in DATA.items():
     if not isinstance(subdirs, tuple):
       subdirs = (subdirs,)
 
     for subdir in subdirs:
       for file in find_files("tree.json", here=f"data/{subdir}"):
         tree = json_read(file)
-        tree = apply(tree, ref_nodes)
+        tree = apply(tree, data)
         json_write(tree, file, no_sort_keys=True)
 
         changelog_path = join(dirname(file), "CHANGELOG.md")
@@ -294,8 +345,11 @@ def main():
           file_write(f"## Unreleased\n\nTest\n\n{changelog}", changelog_path)
 
 
-def apply(tree, ref_nodes):
-  dict_set(tree, ["meta", "extensions", "nextclade", "ref_nodes"], ref_nodes)
+def apply(tree, data):
+  if "ref_nodes" in data:
+    dict_set(tree, ["meta", "extensions", "nextclade", "ref_nodes"], data["ref_nodes"])
+  if "clade_node_attrs" in data:
+    dict_set(tree, ["meta", "extensions", "nextclade", "clade_node_attrs"], data["clade_node_attrs"])
   return tree
 
 
